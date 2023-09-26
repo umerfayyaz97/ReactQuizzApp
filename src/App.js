a95527f807d1d7f6a50347a59eef2a59d55f4dd1
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2023-09-26 15:04:46 +0500

    Changes to Data structure

--- src/App.js
+++ src/App.js
@@ -2,33 +2,31 @@ import react, { useState } from "react";
 import questions from "./data/data.json";
 
 export default function App() {
-  let [selectedOption, setSelectedOption] = useState("");
+  const [showResult, setShowResult] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
 
-  const onClickSelect = (selectedOption) => {
-    setSelectedOption(selectedOption);
-    console.log(selectedOption);
-  };
   return (
     <>
-      <h1 className="px-4 py-3 text-center text-2xl bg-teal-500 text-white/95 font-bold rounded-t-md">
+      <h1 className="px-4 py-3 text-2xl font-bold text-center bg-teal-500 text-white/95 rounded-t-md">
         General Knowledge Quiz
       </h1>
       <br />
       {questions.map((q) => (
         <div key={q.id}>
-          <div className="flex flex-col justify-center items-center">
-            <div className="bg-white rounded-md pb-2 shadow-lg">
-              <p className="bg-teal-700 px-4 py-3 text-white font-bold rounded-t-md">
+          <div className="flex flex-col items-center justify-center">
+            <div className="pb-2 bg-white rounded-md shadow-lg">
+              <p className="px-4 py-3 font-bold text-white bg-teal-700 rounded-t-md">
                 {q.id + 1}: {q.question}
               </p>
-              <div className="flex flex-col w-full gap-3 pt-3 pb-2  px-2 relative">
+              <div className="relative flex flex-col w-full gap-3 px-2 pt-3 pb-2">
                 {q.options.map((options) => (
                   <div key={options}>
                     <div
                       className={
                         options === selectedOption
-                          ? " visited: bg-black"
-                          : "relative w-full h-8"
+                          ? " visited: bg-lime-200 "
+                          : "relative w-full h-8 "
                       }
                       onClick={() => onClickSelect(options)}
                     >
@@ -37,9 +35,8 @@ export default function App() {
                   </div>
                 ))}
               </div>
-              <div className="w-full flex flex-row justify-end px-2 pt-1">
-                <button className="bg-emerald-500 w-full rounded-md py-2 text-white font-bold hover:bg-emerald-600 transition-all duration-300">
-                  {/* <button className=" relative px-2.5 py-1.5 border center border-green-500 rounded text-sm text-green-500 overflow-hidden hover:shadow-lg active:top-0.5 active:outline-none focus:outline-none"> */}
+              <div className="flex flex-row justify-end w-full px-2 pt-1">
+                <button className="w-full py-2 font-bold text-white transition-all duration-300 rounded-md bg-emerald-500 hover:bg-emerald-600">
                   Check Answer
                 </button>
               </div>
